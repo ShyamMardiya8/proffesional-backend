d7d58672a4282522c50d8076eb8f35f5d0450bd0
ShyamMardiya8 <shyammardiya8@gmail.com>
2026-01-29 11:12:21 +0530

    Add user and video models, update environment configuration, and enhance app structure

--- src/constatnts.js
+++ src/constatnts.js
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const DB_NAME = "videotube";
 
 export const CORS_OPTIONS = {
   origin: process.env.CORS_ORIGIN,
   credentials: true,
 };
+
+export const PORT = process.env.PORT;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import app from "./app.js";
+import { PORT } from "./constatnts.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => console.log("Mongodb connection failed"));
