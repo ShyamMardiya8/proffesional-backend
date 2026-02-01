d59b6dd03e21f5774245479ff366785d4a7d9700
ShyamMardiya8 <shyammardiya8@gmail.com>
2026-02-01 12:12:45 +0530

    Add multer and cloudinary integration, create middleware and service files, and implement ApiResponse utility

---
+++ src/services/cloudinary.service.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadFileCloudinary = async (fileUploadPath) => {
+  try {
+    if (!fileUploadPath) return null;
+    const response = await cloudinary.uploader.upload(fileUploadPath, {
+      resource_type: "auto",
+    });
+    console.log("cloudinary file uploaded successfully", response);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(fileUploadPath);
+    return null;
+  }
+};
+
+export { uploadFileCloudinary };
